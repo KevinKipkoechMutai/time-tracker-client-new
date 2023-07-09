ccca02a02742dc68d64a040c92fe6e343fe006c5
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-07-09 15:55:57 +0300

    build the home component

--- src/scenes/Navbar/index.jsx
+++ src/scenes/Navbar/index.jsx
@@ -1,7 +1,49 @@
+import { useTheme, Typography, Box } from "@mui/material"
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import AccessTimeFilledIcon from "@mui/icons-material/AccessTimeFilled"
+import FlexBetween from "../../components/FlexBetween"
 
 const Navbar = () => {
+  const { palette } = useTheme()
+  const [selected, setSelected] = useState("home")
+
   return (
-    <div>Navbar</div>
+    <FlexBetween mb="0.25rem" p="0.5rem 0" color={palette.grey[300]}>
+      {/* Left End */}
+      <FlexBetween gap="0.75rem">
+        <AccessTimeFilledIcon sx={{ fontSize: "28px" }}/>
+        <Typography variant="h4" fontSize="16px">Task-Timer</Typography>
+      </FlexBetween>
+
+      {/* RIGHT END */}
+      <FlexBetween gap="2rem">
+        <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
+          <Link
+            to="/"
+            onClick={() => setSelected("home")}
+            style={{
+              color: selected === "home" ? "inherit" : palette.grey[700],
+              textDecoration: "inherit"
+            }}
+          >
+            Home
+          </Link>
+        </Box>
+        <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
+          <Link
+            to="/dashboard"
+            onClick={() => setSelected("dashboard")}
+            style={{
+              color: selected === "dashboard" ? "inherit" : palette.grey[700],
+              textDecoration: "inherit"
+            }}
+          >
+            Dashboard
+          </Link>
+        </Box>
+      </FlexBetween>
+    </FlexBetween>
   )
 }
 
--- src/scenes/home/index.jsx
+++ src/scenes/home/index.jsx
@@ -1,9 +1,51 @@
+/* eslint-disable react/prop-types */
+import { Box, Button, Typography, useTheme } from "@mui/material"
+import HomeHero from "../../components/HomeHero"
+import TaskContainer from "../../components/TaskContainer"
+import TaskCard from "../../components/TaskCard"
+import FlexBetween from "../../components/FlexBetween"
+
+import { useMemo } from "react"
 
 
-const Home = () => {
+const Home = ({ data }) => {
+  const { palette } = useTheme()
+
+
+  const tasks = useMemo(() => {
+    return (
+      data &&
+      data.map(({ id, name, timeSpent, taskType, startDate}, index) => {
+        const formattedStartDate = startDate.toLocaleString()
+
+        return (
+        <TaskCard 
+          key={`Task-${index}`}
+          name={name}
+          id={id}
+          timeSpent={timeSpent}
+          taskType={taskType}
+          startDate={formattedStartDate}
+        />
+        )
+      })
+    )
+  }, [data])
+
   return (
-    <div>Home</div>
-  )
+    <Box width="100%" height="100%">
+      <HomeHero />
+      <Box width="40%" margin="auto" mt="2rem" mb="1rem">
+        <FlexBetween gap="2rem">
+          <Typography variant="h1" color={palette.grey[700]}>My Tasks</Typography>
+          <Button variant="contained">+ Create Task</Button>
+        </FlexBetween>
+      </Box>
+      <TaskContainer width="100%" p="1.5rem" display="flex" flexDirection="column" gap="0.3rem">
+        {tasks}
+      </TaskContainer>
+    </Box>
+  ) 
 }
 
 export default Home
